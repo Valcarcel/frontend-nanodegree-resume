699324d7be1b00c1d477e1d3cf937abe1c3a6ed1
Valcarcel <ramon.widmer@gmail.com>
2015-04-14 20:47:10 -0700

    This resume in progress works in lesson 2 work quiz.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,32 +1,4 @@
-
-//var awesomeThoughts = "I am Ramon and I am AWESOME!";
-
-//var funThoughts = awesomeThoughts.replace("AWESOME", "FUN")
-
-//console.log(funThoughts);
-
-//$("#main").append(funThoughts);
-
-//var name = "Ramon Widmer";
-//var formattedName = HTMLheaderName.replace("%data%", name);
-
-//ar role = "Network Engineer";
-//var formattedRole = HTMLheaderRole.replace("%data%",role);
-
-//$("#header").prepend(formattedRole);
-//$("#header").prepend(formattedName);
-
-
-//var s = "audacity";
-//s = s.slice(4);
-//console.log(s);
-
-//var fruits = [];
-//fruits.push('banana', 'apple', 'peach', 'prune');
-//console.log(Object.keys(fruits));
-//console.log(fruits.length);
-
-//var skills = [“awesomeness”, “programming”, “teaching”];
+// work, projects, bio, edu
 
 var bio = {
 	"name" : "Ramon Widmer",
@@ -37,34 +9,91 @@ var bio = {
 	},
 	"Welcomemessage" : "Hello, this is my welcome message.",
 	"skills": [
-		"awesmomeness", "programming"
+		"Network Enineering", "Capacity Planning", "PERL"
 	],
 	"bioPic": "images/fry.jpg"
 }
 
-// good up to here.
 
-var WorkExp = {
-	"Position" : "Senior Network Engineer",
-	"Employer" : "AT&T",
-	"YearWorked" : 14,
-	"WorkCity" : "San Ramon"
+ var Education = {
+	"schools": [
+		{
+			"name": "UC Davis",
+			"city": "Davis, California",
+			"degree": "BS",
+			"major": "biological sciences"
+		},
+		{
+			"name": "San Joaquin Delta College",
+			"city": "Stockton, California",
+			"degree": "AA"
+		}
+	],
+	"onlineCourses" : [
+		{
+			"title": "Intro to HTML and CSS",
+			"school": "Udacity",
+			"dates": 2015,
+			"url": "https://www.udacity.com/course/progress#!/c-ud304-nd"
+		}
+	]
 }
 
-var Education = {}
-Education["school1"] = "UC Davis";
-Education["years"] = 4;
-Education["City"] = "Davis, California";
 
 
-$("#main").prepend(bio.name);
+var work = {
+	"jobs": [	// jobs array in the work object
+		{
+		"employer": "AT&T",
+		"title": "Senior Network Design Engineer",
+		"dates": "September 2010",
+		"Description": "Performed capacity management for the ATT mobility newwork. Troubleshoot network connectivity issues. Wroter router and switch configs for Juniper and Cisco routers and switches. Built engineering design packages for the installation of new network hardware. Used PERL programming to automate troubleshooting, and the development of router command line configurations. Managed POR in order to have forecasted router builds land on month over month budget."
+		},
+		{
+		"employer": "PacBell Wireless",
+		"title": "Datacom Engineer",
+		"dates": "September 2003",
+		"Description": "Operations, monitoring, IP addree management, etc."
+		}
+	]
+}
+
+var projects = {
+	"projects": [
+		{
+			"title": "Sample project 1",
+			"dates": "2014",
+			"description": "Website that automated network troubleshooting."
+		}
+	]
+}
+
+//$("#main").prepend(bio.name);
 //$("#main").prepend(bio.role);
 //$("#main").prepend(bio.Welcomemessage);
 //$("#main").prepend(WorkExp.Position);
 //$("#main").append(Education.school1);
-$("#main").append(Education["school1"]);
-$("#main").prepend(Education.years);
-$("#main").append(Education.City);
+//$("#main").append(Education["school1"]);
+//$("#main").prepend(Education.years);
+//$("#main").append(Education.City);
+
+// Can I acces the work object?
 
 
+
+
+for (job in work.jobs) {
+
+	$("#workExperience").append(HTMLworkStart);
+
+	var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
+	//console.log(formattedEmployer);
+
+	var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[job].title);
+	var formattedEmployerTitle = formattedEmployer + formattedTitle;
+	console.log(formattedEmployerTitle);
+	//$("#workExperience").append(formattedEmployerTitle);
+	$(".work-entry:last").append(formattedEmployerTitle);
+	//$("#main").append(formattedEmployerTitle);
+}
 
